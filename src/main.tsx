3b8320c57618cbf418cb1e33a4634255fd5699ec
DouglasB <douglas_borges09@hmail.com>
2024-09-22 20:21:54 -0300

    clean code

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { RouterProvider } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// import { App } from "./App";
 import { router } from "./routes";
 
 const queryClient = new QueryClient();
